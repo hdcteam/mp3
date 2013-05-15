085f56ba749dcbe2adffc56e1f6d849bcb8bdeca
Mai Hoang Hai <hoanghai27@gmail.com>
2013-05-15 17:47:25 +0700

    Thêm PlaylistCategory

--- MP3/App_Start/RouteConfig.cs
+++ MP3/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace MP3
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Video",
+                url: "Video/{id}/{title}",
+                defaults: new { controller = "Category", action = "ListVideo", title = UrlParameter.Optional, id = UrlParameter.Optional, page = 1 }
+            );
+
+            routes.MapRoute(
+                name: "Video with page",
+                url: "Video/{id}/{title}/{page}",
+                defaults: new { controller = "Category", action = "ListVideo", title = UrlParameter.Optional, id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Play music",
                 url: "Play/{id}/{title}",
                 defaults: new { controller = "Song", action = "Details", name = UrlParameter.Optional, id = UrlParameter.Optional }
--- MP3/Controllers/CategoryController.cs
+++ MP3/Controllers/CategoryController.cs
@@ -126,6 +126,28 @@ namespace MP3.Controllers
             return View(category.Songs.Where(s => s.VideoFile == null).ToPagedList(pageNumber, pageSize));
         }
 
+        //
+        // GET: /Category/ListVideo
+
+        public ActionResult ListVideo(int id, string title, int? page)
+        {
+            Categories category = db.Categories.Find(id);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            ViewBag.id = id;
+            ViewBag.categoryTitle = title;
+            ViewBag.category = category;
+            return View(category.Songs.Where(s => s.MusicFile == null).ToPagedList(pageNumber, pageSize));
+        }
+
+        //
+        // GET: /Album/ListVideo
+
+        public ActionResult ListAlbum(int id, string title, int? page)
+        {
+            return View();
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
