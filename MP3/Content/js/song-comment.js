e084e804f9d7d2aa7b83e1c5fbb85a5d50ab30b1
Mai Hoang Hai <hoanghai27@gmail.com>
2013-05-17 16:34:04 +0700

    Chuyển JS ra file riêng

---
+++ MP3/Content/js/song-comment.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+
+    $(".new-comment form").submit(function () {
+        this.submit.disabled = true;
+        this.submit.value = "Đang tải ...";
+        var form = this;
+        $.ajax(
+            {
+                url: '@Url.Action("Create", "SongComment")', 
+                data: {songId: @Model.Id, content: this.cmtContent.value}, 
+                success: function (data) {
+
+                    if (data.type == "SUCCESS")
+                    {
+                        $(".new-comment").hide();
+                        $(".comment-list").append(data.html);
+                        $(".no-comment").hide();
+                    }
+                    else
+                    {
+                        alert("Lỗi" + data.msg);
+                        form.submit.disabled = false;
+                        form.submit.value = "Đăng bình luận";
+                    }
+                },
+
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert(textStatus + ":" + jqXHR.responseText );
+                    form.submit.disabled = false;
+                    form.submit.value = "Đăng bình luận";
+                },
+
+                dataType: "json",
+                type: "POST"
+            });
+        return false;
+    });
+});
---
+++ MP3/Content/js/song-like.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $(".tools .like-btn").click(function () {
+        if (this.innerText == "Thích")
+            this.innerText = "Bỏ thích";
+        else
+            this.innerText = "Thích";
+    });
+});
