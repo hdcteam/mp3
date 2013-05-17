cff395bec6aa0fd0a636c7573330e0b8a72e1eef
Mai Hoang Hai <hoanghai27@gmail.com>
2013-05-17 16:37:27 +0700

    Xóa bỏ các Action và View không dùng đến

--- MP3/Controllers/SongCommentController.cs
+++ MP3/Controllers/SongCommentController.cs
@@ -16,38 +16,6 @@ namespace MP3.Controllers
         private Entities db = new Entities();
 
         //
-        // GET: /SongComment/
-
-        public ActionResult Index()
-        {
-            var songcomment = db.SongComment.Include(s => s.Song).Include(s => s.User);
-            return View(songcomment.ToList());
-        }
-
-        //
-        // GET: /SongComment/Details/5
-
-        public ActionResult Details(int id = 0)
-        {
-            SongComment songcomment = db.SongComment.Find(id);
-            if (songcomment == null)
-            {
-                return HttpNotFound();
-            }
-            return View(songcomment);
-        }
-
-        ////
-        //// GET: /SongComment/Create
-
-        //public ActionResult Create()
-        //{
-        //    ViewBag.SongId = new SelectList(db.Songs, "Id", "Name");
-        //    ViewBag.UserId = new SelectList(db.UserProfile, "UserId", "UserName");
-        //    return View();
-        //}
-
-        //
         // POST: /SongComment/Create
 
         [HttpPost]
@@ -90,65 +58,6 @@ namespace MP3.Controllers
             jsonResult.ContentEncoding = System.Text.Encoding.UTF8;
 
             return jsonResult;
-        }
-
-        //
-        // GET: /SongComment/Edit/5
-
-        public ActionResult Edit(int id = 0)
-        {
-            SongComment songcomment = db.SongComment.Find(id);
-            if (songcomment == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.SongId = new SelectList(db.Songs, "Id", "Name", songcomment.SongId);
-            ViewBag.UserId = new SelectList(db.UserProfile, "UserId", "UserName", songcomment.UserId);
-            return View(songcomment);
-        }
-
-        //
-        // POST: /SongComment/Edit/5
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(SongComment songcomment)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(songcomment).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.SongId = new SelectList(db.Songs, "Id", "Name", songcomment.SongId);
-            ViewBag.UserId = new SelectList(db.UserProfile, "UserId", "UserName", songcomment.UserId);
-            return View(songcomment);
-        }
-
-        //
-        // GET: /SongComment/Delete/5
-
-        public ActionResult Delete(int id = 0)
-        {
-            SongComment songcomment = db.SongComment.Find(id);
-            if (songcomment == null)
-            {
-                return HttpNotFound();
-            }
-            return View(songcomment);
-        }
-
-        //
-        // POST: /SongComment/Delete/5
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            SongComment songcomment = db.SongComment.Find(id);
-            db.SongComment.Remove(songcomment);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
--- MP3/Controllers/SongController.cs
+++ MP3/Controllers/SongController.cs
@@ -14,15 +14,6 @@ namespace MP3.Controllers
         private Entities db = new Entities();
 
         //
-        // GET: /Song/
-
-        public ActionResult Index()
-        {
-            var songs = db.Songs.Include(s => s.Country);
-            return View(songs.ToList());
-        }
-
-        //
         // GET: /Song/Details/5
 
         public ActionResult Details(string title, int id = 0)
@@ -36,90 +27,6 @@ namespace MP3.Controllers
                 return HttpNotFound();
             }
             return View(song);
-        }
-
-        //
-        // GET: /Song/Create
-
-        public ActionResult Create()
-        {
-            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name");
-            return View();
-        }
-
-        //
-        // POST: /Song/Create
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Songs song)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Songs.Add(song);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", song.CountryId);
-            return View(song);
-        }
-
-        //
-        // GET: /Song/Edit/5
-
-        public ActionResult Edit(int id = 0)
-        {
-            Songs song = db.Songs.Find(id);
-            if (song == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", song.CountryId);
-            return View(song);
-        }
-
-        //
-        // POST: /Song/Edit/5
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Songs songs)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(songs).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", songs.CountryId);
-            return View(songs);
-        }
-
-        //
-        // GET: /Song/Delete/5
-
-        public ActionResult Delete(int id = 0)
-        {
-            Songs songs = db.Songs.Find(id);
-            if (songs == null)
-            {
-                return HttpNotFound();
-            }
-            return View(songs);
-        }
-
-        //
-        // POST: /Song/Delete/5
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Songs songs = db.Songs.Find(id);
-            db.Songs.Remove(songs);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
